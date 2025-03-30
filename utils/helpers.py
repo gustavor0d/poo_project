63e0d0a999cc616800f91cb0aa69fd34dba939a6
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-03-30 13:48:08 -0300

    feat: add date formatting and input validation functions

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,20 @@
 import os
+from datetime import datetime
 
 def cls():
     return os.system("cls")
+
+def valor_entrada(table, prompt):
+    valor = input(prompt)
+    if valor == '0':
+        cls()
+        print(f"\nCadastro de {table} cancelado.")
+        return None
+    return valor
+
+data = datetime.today().date()
+
+def data_format(data_pedido):
+    if not data_pedido:
+        return "Não localizada"
+    return data_pedido.strftime("%d/%m/%Y")
