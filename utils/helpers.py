71ac8b881b551575845765cbb13b029c4df0b7fb
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-04-06 18:38:48 -0300

    style: remove unnecessary character from print statement

--- utils/helpers.py
+++ utils/helpers.py
@@ -8,7 +8,7 @@ def valor_entrada(table, prompt):
     valor = input(prompt).strip()
     if valor == '0':
         cls()
-        print(f"\nCadastro de {table} cancelado.")
+        print(f"Cadastro de {table} cancelado.")
         return None
     return valor
 
